7f5965dc716e7a47fa8d11304c2dd9097a95502a
Richard Nelis de Souza <souza.richard33@hotmail.com>
2021-04-05 21:55:55 -0300

    Update ImagemController.cs

--- FarenayteApi/FarenayteApi.Presentation/Controllers/ImagemController.cs
+++ FarenayteApi/FarenayteApi.Presentation/Controllers/ImagemController.cs
@@ -3,9 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace FarenayteApi.Presentation.Controllers
@@ -13,39 +11,25 @@ namespace FarenayteApi.Presentation.Controllers
     [ApiController, Route("[controller]"), Authorize]
     public class ImagemController : BaseController
     {
-        String pathRoot;
+        private readonly string _pathRoot;
+        private readonly string _imagemFold = "\\Imagens\\";
         public static IWebHostEnvironment _environment;
 
         public ImagemController(IWebHostEnvironment environment)
         {
             _environment = environment;
 
-            if (!Directory.Exists(_environment.ContentRootPath + "\\Imagens\\"))
+            if (!Directory.Exists(_environment.ContentRootPath + _imagemFold))
             {
-                Directory.CreateDirectory(_environment.ContentRootPath + "\\Imagens\\");
+                Directory.CreateDirectory(_environment.ContentRootPath + _imagemFold);
             }
 
-            pathRoot = _environment.ContentRootPath + "\\Imagens\\";
-        }
-
-        [HttpGet]
-        public IEnumerable<IActionResult> Get()
-        {
-            List<FileContentResult> files = new List<FileContentResult>();
-            string[] images = Directory.GetFiles(pathRoot, "*");
-
-            for (int i = 0; i < images.Length; i++)
-            {
-                Byte[] b = System.IO.File.ReadAllBytes(pathRoot + images[i]);   // You can use your own method over here.         
-                files.Add(File(b, "image/" + images[i].Split(".")[1]));
-            }
-
-            return files;
+            _pathRoot = _environment.ContentRootPath + _imagemFold;
         }
 
         public IActionResult Get(String name)
         {
-            Byte[] b = System.IO.File.ReadAllBytes(pathRoot + name);   // You can use your own method over here.         
+            Byte[] b = System.IO.File.ReadAllBytes(_pathRoot + name);   // You can use your own method over here.         
             return File(b, "image/" + name.Split(".")[1]);
         }
 
@@ -58,9 +42,9 @@ namespace FarenayteApi.Presentation.Controllers
                 {
                     for (int i = 0; i < arquivos.Count; i++)
                     {
-                        using (FileStream filestream = System.IO.File.Create(pathRoot + arquivos[i].FileName))
+                        using (FileStream filestream = System.IO.File.Create(_pathRoot + arquivos[i].FileName))
                         {
-                            String caminho = "\\Imagens\\" + arquivos[i].FileName;
+                            String caminho = _imagemFold + arquivos[i].FileName;
                             await arquivos[i].CopyToAsync(filestream);
                             filestream.Flush();
                         }
@@ -72,7 +56,7 @@ namespace FarenayteApi.Presentation.Controllers
                 {
                     for (int i = 0; i < arquivos.Count; i++)
                     {
-                        System.IO.File.Delete(pathRoot + arquivos[i].FileName);
+                        System.IO.File.Delete(_pathRoot + arquivos[i].FileName);
                     }
 
                     return false;
