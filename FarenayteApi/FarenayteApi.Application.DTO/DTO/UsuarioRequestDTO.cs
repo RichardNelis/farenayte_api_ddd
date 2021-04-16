03e6e327711096b3603449aeb643f8428696a0d0
Richard Nelis de Souza <souza.richard33@hotmail.com>
2021-04-16 09:38:06 -0300

    Update UsuarioRequestDTO.cs

--- FarenayteApi/FarenayteApi.Application.DTO/DTO/UsuarioRequestDTO.cs
+++ FarenayteApi/FarenayteApi.Application.DTO/DTO/UsuarioRequestDTO.cs
@@ -5,7 +5,7 @@ namespace FarenayteApi.Application.DTO.DTO
 {
     public class LoginDTO
     {
-        public int Id { get; set; }
+        //public int Id { get; set; }
                 
         public string Email { get; set; }
         
