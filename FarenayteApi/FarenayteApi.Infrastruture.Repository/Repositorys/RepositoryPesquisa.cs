181f6e216fdd0e1a9760afa8da570f4811a0c3b9
Richard Nelis de Souza <souza.richard33@hotmail.com>
2021-05-17 14:12:55 -0300

    Removido where

--- FarenayteApi/FarenayteApi.Infrastruture.Repository/Repositorys/RepositoryPesquisa.cs
+++ FarenayteApi/FarenayteApi.Infrastruture.Repository/Repositorys/RepositoryPesquisa.cs
@@ -23,38 +23,14 @@ namespace FarenayteApi.Infrastruture.Repository.Repositorys
         {           
             double latitude = obj.Latitude;
             double longitude = obj.Longitude;
+
             ICollection<PessoaJuridica> list = await _context.PessoaJuridicas
                 .Include(x => x.Publicacao)                
                 .Where(x => (12742 * Math.Asin(Math.Sqrt(Math.Sin(((Math.PI / 180) * (x.Latitude - latitude)) / 2) * Math.Sin(((Math.PI / 180) * (x.Latitude - latitude)) / 2) +
-                                    Math.Cos((Math.PI / 180) * latitude) * Math.Cos((Math.PI / 180) * (x.Latitude)) *
-                                    Math.Sin(((Math.PI / 180) * (x.Longitude - longitude)) / 2) * Math.Sin(((Math.PI / 180) * (x.Longitude - longitude)) / 2)))) <= 50)
+                                     Math.Cos((Math.PI / 180) * latitude) * Math.Cos((Math.PI / 180) * (x.Latitude)) *
+                                     Math.Sin(((Math.PI / 180) * (x.Longitude - longitude)) / 2) * Math.Sin(((Math.PI / 180) * (x.Longitude - longitude)) / 2)))) <= 50)
                 .ToListAsync();    
-
-            if (!string.IsNullOrEmpty(obj.RazaoSocial))
-            {
-                list = list.Where(x => x.RazaoSocial.Contains(obj.RazaoSocial)).ToList();
-            }
-
-            if (!string.IsNullOrEmpty(obj.NomeFantasia))
-            {
-                list = list.Where(x => x.NomeFantasia.Contains(obj.NomeFantasia)).ToList();
-            }
-
-            if (!string.IsNullOrEmpty(obj.Titulo))
-            {
-                list = list.Where(x => x.Publicacao.Titulo.Contains(obj.Publicacao.Titulo)).ToList();
-            }
-
-            if (!string.IsNullOrEmpty(obj.Descricao))
-            {
-                list = list.Where(x => x.Publicacao.Descricao.Contains(obj.Publicacao.Descricao)).ToList();
-            }
-
-            if (obj.Preco != null && obj.Preco.HasValue)
-            {
-                list = list.Where(x => x.Publicacao.Preco == obj.Publicacao.Preco).ToList();
-            }
-
+            
             return list;
         }
     }
